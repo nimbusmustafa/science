3c354b241fa5b0e9524c0c93f877a9e42d199b1f
Mustafa Haji <117943931+nimbusmustafa@users.noreply.github.com>
2024-01-12 20:36:07 +0530

    Add files via upload

---
+++ science/site2.py
@@ -0,0 +1,50 @@
+import os
+# from PIL import Image
+import cv2
+# cap = cv2.VideoCapture(2)
+key_pressed = False
+image = None
+images = []
+count1 = 0
+def load_images_from_folder(folder_path):
+    images = []
+    filenames = sorted(os.listdir(folder_path))  
+    for filename in filenames:
+        if filename.endswith(('.jpg', '.jpeg', '.png', '.gif')):  
+            image_path = os.path.join(folder_path, filename)
+            img = cv2.imread(image_path)
+            images.append(img)
+    return images
+
+folder_path = '/home/mustafa/science/site2'
+images = load_images_from_folder(folder_path)
+
+
+stitcher = cv2.Stitcher_create()
+for i in range(len(images)-3):
+         for j in range(i+1, len(images)-2):
+            status, result = stitcher.stitch([images[i], images[j], images[j+1], images[j+2]])
+
+            if status == 0:
+                stitched_image = result
+                h, w = stitched_image.shape[:2]
+
+                # print(f"Stitched image dimensions: {w} x {h}")
+
+                if 3 * h > w:
+                    cropped_image = stitched_image[int((h - w / 3) / 2):int((h - w / 3) / 2 + (w / 3)), :]
+                elif 3 * h < w:
+                    cropped_image = stitched_image[:, int((w - 3 * h) / 2): int(((w - 3 * h) / 2 + w))]
+                else:
+                    cropped_image = stitched_image
+
+                cropped_filename = f"{folder_path}/final_{count1}.jpg"  
+                # print(f"Cropped image dimensions: {cropped_image.shape[0]} x {cropped_image.shape[1]}")
+                cv2.imwrite(cropped_filename, cropped_image)
+                print(f"Cropped image saved as {cropped_filename} for {i} with {j} and {j+1}")
+
+                count1 += 1
+
+            else:
+                print("Image stitching failed.")
+
---
+++ science/textonimg1.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+
+def add_overlay(image, angle, lat, long, elevation):
+    color = (0, 0, 255)
+
+    image = cv2.putText(image, 'N', (102, 45), cv2.FONT_HERSHEY_TRIPLEX,
+                        1.5, color, 1, cv2.LINE_AA)
+    image = cv2.putText(image, 'W', (20, 125), cv2.FONT_HERSHEY_TRIPLEX,
+                        1.5, color, 1, cv2.LINE_AA)
+    image = cv2.putText(image, 'E', (180, 125), cv2.FONT_HERSHEY_TRIPLEX,
+                        1.5, color, 1, cv2.LINE_AA)
+    image = cv2.putText(image, 'S', (102, 205), cv2.FONT_HERSHEY_TRIPLEX,
+                        1.5, color, 1, cv2.LINE_AA)
+
+    image = cv2.putText(image, f'GPS: Latitude -{lat}', (230, 135), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0, 255, 0), 2, cv2.LINE_AA)
+    image = cv2.putText(image, f'GPS: Longitude -{long}', (230, 95), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0, 255, 0), 2, cv2.LINE_AA)
+    image = cv2.putText(image, f'Elevation: -{elevation}', (230, 55), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0, 255, 0), 2, cv2.LINE_AA)
+
+    overlay_image = cv2.imread('needle2.png', cv2.IMREAD_UNCHANGED)
+    overlay_image = cv2.resize(overlay_image, (130, 130))
+    row, col, _ = overlay_image.shape
+    center = tuple(np.array([row, col]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(center, (-1.0) * angle, 1.0)
+    overlay_image = cv2.warpAffine(overlay_image, rot_mat, (col, row))
+
+    x_pos = 52
+    y_pos = 43
+
+    needle_image = image.copy()
+
+    for y in range(overlay_image.shape[0]):
+        for x in range(overlay_image.shape[1]):
+            if overlay_image[y, x, 3] > 0:
+                needle_image[y + y_pos, x + x_pos, 0] = overlay_image[y, x, 0]
+                needle_image[y + y_pos, x + x_pos, 1] = overlay_image[y, x, 1]
+                needle_image[y + y_pos, x + x_pos, 2] = overlay_image[y, x, 2]
+
+    overlay_image1 = cv2.imread('MRM_logo.png', cv2.IMREAD_UNCHANGED)
+    overlay_image1 = cv2.resize(overlay_image1, (100, 100))
+
+    x_pos = (image.shape[1]) - overlay_image1.shape[1] - 20
+    y_pos = 30
+
+    mrm_image = needle_image.copy()
+
+    for y in range(overlay_image1.shape[0]):
+        for x in range(overlay_image1.shape[1]):
+            if overlay_image1[y, x, 3] > 0:
+                mrm_image[y + y_pos, x + x_pos, 0] = overlay_image1[y, x, 0]
+                mrm_image[y + y_pos, x + x_pos, 1] = overlay_image1[y, x, 1]
+                mrm_image[y + y_pos, x + x_pos, 2] = overlay_image1[y, x, 2]
+
+    return mrm_image
+
+# Example usage:
+input_image = cv2.imread('final_4.jpg')  
+output_image = add_overlay(input_image, angle=45, lat=37.7749, long=-122.4194, elevation=50)
+
+cv2.imshow('Output Image', output_image)
+cv2.imwrite('site1_img.png', output_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
